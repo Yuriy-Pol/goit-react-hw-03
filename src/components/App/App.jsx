975ce1f89bdf469a3f1a2dee4b9f1fa191c43e33
Yuriy Polozhevets <polozhevetsyura@gmail.com>
2024-02-25 21:46:43 +0200

    add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,63 +1,68 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
-import Options from '../Options/Options';
-import Feedback from '../Feedback/Feedback';
-import Notification from '../Notification/Notification';
-import Description from '../Description/Description';
+import { nanoid } from 'nanoid';
+import { useState, useEffect } from 'react';
+import ContactForm from '../ContactForm/ContactForm';
+import SearchBox from '../SearchBox/SearchBox';
+import ContactList from '../ContactList/ContactList';
+import css from './App.module.css';
 
-const App = () => {
-  const initialState = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+const data = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
-  const [feedback, setFeedback] = useState(() => {
-    const savedFeedback = JSON.parse(localStorage.getItem('feedback'));
-    return savedFeedback ? savedFeedback : initialState;
-  });
-
-  useEffect(() => {
-    localStorage.setItem('feedback', JSON.stringify(feedback));
-  }, [feedback]);
-
-  const updateFeedback = feedbackType => {
-    setFeedback(prevFeedback => ({
-      ...prevFeedback,
-      [feedbackType]: prevFeedback[feedbackType] + 1,
-    }));
-  };
-
-  const resetFeedback = () => {
-    setFeedback(initialState);
-  };
-
-  const { good, neutral, bad } = feedback;
-  const totalFeedback = good + neutral + bad;
-  const positivePercentage = Math.round(
-    ((good + neutral) / totalFeedback) * 100
-  );
-
-  const hasFeedback = totalFeedback > 0;
-
-  return (
-    <div>
-      <Description />
-      <Options
-        updateFeedback={updateFeedback}
-        resetFeedback={resetFeedback}
-        hasFeedback={hasFeedback}
-      />
-      {hasFeedback && (
-        <Feedback
-          feedback={feedback}
-          totalFeedback={totalFeedback}
-          positivePercentage={positivePercentage}
-        />
-      )}
-      {!hasFeedback && <Notification message="No feedback yet" />}
-    </div>
-  );
+const initialValues = {
+  id: '',
+  name: '',
+  number: '',
 };
 
-export default App;
+export default function App() {
+  const [values, setValues] = useState(() => {
+    const saveStorage = localStorage.getItem('localData');
+    return saveStorage ? JSON.parse(saveStorage) : data;
+  });
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSubmit = (value, actions) => {
+    setValues([...values, { ...value, id: nanoid() }]);
+    actions.resetForm();
+  };
+
+  useEffect(() => {
+    values !== data
+      ? localStorage.setItem('localData', JSON.stringify(values))
+      : '';
+  }, [values]);
+
+  const handlerDelete = idToDelete => {
+    setValues(values.filter(value => value.id !== idToDelete));
+  };
+
+  useEffect(() => {
+    values !== data && values.length !== 0
+      ? localStorage.setItem('localData', JSON.stringify(values))
+      : '';
+  }, [values]);
+
+  const handleSearch = event => {
+    setSearchValue(event.target.value);
+  };
+
+  const Filtered =
+    searchValue.trim() !== ''
+      ? values.filter(value =>
+          value.name.toLowerCase().includes(searchValue.toLowerCase().trim())
+        )
+      : values;
+
+  return (
+    <div className={css.wrapper}>
+      <h1>Phonebook</h1>
+      <ContactForm initialValues={initialValues} onAdd={handleSubmit} />
+      <SearchBox inputValue={searchValue} onChange={handleSearch} />
+      <ContactList contacts={Filtered} onDelete={handlerDelete} />
+    </div>
+  );
+}
--- src/components/Notification/Notification.jsx
+++
@@ -1,4 +0,0 @@
-const Notification = ({ message }) => {
-  return <p>{message}</p>;
-};
-export default Notification;
